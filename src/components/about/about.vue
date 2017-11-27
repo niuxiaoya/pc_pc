4f3886fd21879ac3d439d90476b35543bdaf141d
e <884654718@qq.com>
2017-11-27 21:35:21 +0800

    关于我们

---
+++ src/components/about/about.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+      
+  </div>
+</template>
+<script>
+  export default {
+
+  }
+</script>
+<style lang="less" scoped type="text/less">
+  .story {
+    min-width: 1024px;
+    max-width: 1200px;
+  }
+</style>
+<style type="text/less" lang="less">
+
+</style>
